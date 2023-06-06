ef945d15c5922a2e185c93bc3aa04c046e04c049
Pirania69 <kanashevsky@mail.ru>
2023-06-06 13:38:32 +0300

    ДЗ

---
+++ src/main/java/com/example/testcode/exceptions/IllegalArgumentException.java
@@ -0,0 +1,9 @@
+package com.example.testcode.exceptions;
+
+/*public class IllegalArgumentException extends RuntimeException {
+
+    public IllegalArgumentException(String message) {
+        super(message);
+    }
+
+}*/
